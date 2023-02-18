c500c820cf3d4c16a12f1c2ed2918a9437c9d9ac
Adam <aknochow@Adams-MacBook-Pro.local>
2023-02-17 22:38:35 -0500

    updating to v2

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Shell script wrapper to prepare and deploy in one step..."
-
 ansible-galaxy collection install -r requirements.yaml
 
 ansible-playbook prepare.yaml
-
-ansible-playbook install-awx.yaml
